694d9c4553528a5d10f2b82ee500a878c96d8f9e
aaaaayushh <aayushshah1142@gmail.com>
2021-03-31 14:43:06 +0530

    v1 ready

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./Row.css";
 import axios from "./axios";
-
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-
+  const [trailerUrl, settrailerUrl] = useState("");
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchURL);
@@ -15,6 +16,31 @@ function Row({ title, fetchURL, isLargeRow }) {
     fetchData();
   }, [fetchURL]);
 
+  const opts = {
+    height: "390",
+    widht: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  function handleClick(movie) {
+    if (trailerUrl) {
+      settrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          //https://www.youtube.com/watch?v=XtMThy8QKqU&list=PL-J2q3Ga50oMQa1JdSJxYoZELwOJAXExP&index=5
+          //getting the video id (v=X... in the above url)
+          const urlParams = new URLSearchParams(new URL(url).search);
+          console.log(urlParams);
+          settrailerUrl(urlParams.get("v"));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
   //   console.table(movies);
   return (
     <div className="row">
@@ -23,6 +49,7 @@ function Row({ title, fetchURL, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -31,6 +58,7 @@ function Row({ title, fetchURL, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
